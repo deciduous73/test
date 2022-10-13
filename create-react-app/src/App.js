2815d37c63d3abab532aba58d3215bb75d6f6d8b
easy <easylive@shaoniandeMacBook-Air.local>
2022-10-13 15:00:20 +0800

    11

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React111
+          Learn React1112222
         </a>
       </header>
     </div>
