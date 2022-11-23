6cc300a8b08553bcb4b3387daed79930e097a801
easy <958675469@qq.com>
2022-11-23 12:50:48 +0800

    11.23

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reac1111888833344
+          11.23
         </a>
       </header>
     </div>
